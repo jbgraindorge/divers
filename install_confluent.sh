d206a0a3857da1daa1f8a252c39b0e34fe481c10
jbgraindorge <jb.graindorge@gmail.com>
2017-11-22 11:00:25 +0100

    Update install_confluent.sh

--- install_confluent.sh
+++ install_confluent.sh
@@ -20,6 +20,8 @@ install_confluent()
   yum clean all
 	echo "Installing confluent"
 	yum -y -q install confluent-platform-oss-2.11
+	confluent start
+	confluent stop connect
 
 	#echo "Ensuring Cassandra starts on boot"
 	#/sbin/chkconfig --add cassandra
@@ -31,16 +33,27 @@ install_confluent()
 
 install_twitter_connector()
 {
+  mkdir -p /usr/local/share/kafka/plugins/
+  ##INSTALL FIRST REPO
+  cd /root/
+  git clone https://github.com/jcustenborder/kafka-connect-twitter.git
+  mv kafka-connect-twitter kafka-connect-twitter-bad
+  cd kafka-connect-twitter-bad
+  mvn clean package
+  cp /usr/local/kafka-connect-twitter/target/kafka-connect-target/usr/share/kafka-connect/kafka-connect-twitter/* /usr/local/share/kafka/plugins/
+  ##INSTALL SECOND REPO
   cd /root/
   git clone https://github.com/Eneco/kafka-connect-twitter.git
   cd kafka-connect-twitter/
   mvn clean package
   cp twitter-source.properties.example twitter-source.properties
+  ##INSTALL SOME MISSING
   cd /root/
   wget http://central.maven.org/maven2/com/twitter/joauth/6.0.2/joauth-6.0.2.jar
   git clone https://github.com/twitter/hbc.git
   cd hbc
   mvn install
+  echo "export CLASSPATH=/root/kafka-connect-twitter/target/kafka-connect-twitter-0.1-jar-with-dependencies.jar:/root/kafka-connect-twitter/target/:/root/kafka-connect-twitter/target/classes/:/usr/share/java/httpcomponents/httpclient.jar:/root/hbc/hbc-core/target/classes/:/root/hbc/hbc-example/target/classes/:/root/hbc" >> /root/.bashrc
 }
 
 ensure_system_updated()

d9ebc7d48c8287e8eb956005e2a8217a576856dc
jbgraindorge <jb.graindorge@gmail.com>
2017-11-22 06:13:32 +0100

    Create install_confluent.sh

---
+++ install_confluent.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+install_dependencies()
+{
+	echo "Installing Java 1.8 (openjdk)"
+	yum -y install java-1.8.0-openjdk
+}
+
+install_confluent()
+{
+	echo "Adding YUM Repo for Confluent"
+	cd /etc/yum.repos.d/
+	wget https://raw.githubusercontent.com/jbgraindorge/divers/master/confluent.repo
+  yum clean all
+	echo "Installing confluent"
+	yum -y -q install confluent-platform-oss-2.11
+
+	#echo "Ensuring Cassandra starts on boot"
+	#/sbin/chkconfig --add cassandra
+	#/sbin/chkconfig cassandra on
+
+	#echo "Starting Cassandra"
+	#systemctl start cassandra
+}
+
+ensure_system_updated()
+{
+	#yum makecache fast
+
+	echo "Updating Operating System"
+	#yum -y -q update
+}
+
+install_dependencies
+install_confluent
+ensure_system_updated

2010567f0b1f1dcc1024279a4fc01f6a541cb0ce
jbgraindorge <jb.graindorge@gmail.com>
2017-11-23 10:22:15 +0100

    Update scriptCassandraProvisioner.sh

--- scriptCassandraProvisioner.sh
+++ scriptCassandraProvisioner.sh
@@ -34,7 +34,7 @@ install_ruby()
 yum -y -q install gcc-c++ patch readline readline-devel zlib zlib-devel libyaml-devel libffi-devel openssl-devel make bzip2 autoconf automake libtool bison iconv-devel ruby-devel libxml2 libxml2-devel libxslt libxslt-devel git
 gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -sSL https://get.rvm.io | bash -s stable --ruby
-source /usr/local/rvm/scripts/rvm
+#source /usr/local/rvm/scripts/rvm
 gem install bundler
 gem install cassandra-web
 nohup cassandra-web &

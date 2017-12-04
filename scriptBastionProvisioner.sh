38470330901d1e85e2b5367c7f454e80e4d642ac
jbgraindorge <jb.graindorge@gmail.com>
2017-12-04 14:17:23 +0100

    Update scriptBastionProvisioner.sh

--- scriptBastionProvisioner.sh
+++ scriptBastionProvisioner.sh
@@ -64,12 +64,12 @@ EOF
 
 cat > /home/ubuntu/ansible-hadoop/inventory/static <<\EOF
 [master-nodes]
-'${master_node1}' ansible_ssh_user=ubuntu ansible_python_interpreter=/usr/bin/python3
-'${master_node2}' ansible_ssh_user=ubuntu ansible_python_interpreter=/usr/bin/python3
+10.0.1.55 ansible_ssh_user=ubuntu ansible_python_interpreter=/usr/bin/python3
+10.0.1.56 ansible_ssh_user=ubuntu ansible_python_interpreter=/usr/bin/python3
 [slave-nodes]
-'${slave_node1}' ansible_ssh_user=ubuntu ansible_python_interpreter=/usr/bin/python3
-'${slave_node2}' ansible_ssh_user=ubuntu ansible_python_interpreter=/usr/bin/python3
-'${slave_node3}' ansible_ssh_user=ubuntu ansible_python_interpreter=/usr/bin/python3
+10.0.1.65 ansible_ssh_user=ubuntu ansible_python_interpreter=/usr/bin/python3
+10.0.1.66 ansible_ssh_user=ubuntu ansible_python_interpreter=/usr/bin/python3
+10.0.1.67 ansible_ssh_user=ubuntu ansible_python_interpreter=/usr/bin/python3
 EOF
 
 ansible all -b -m copy -a "src=/tmp/hosts dest=/etc/hosts"
